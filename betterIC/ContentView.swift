24e88e858681386609185a0d864d416b54cbbdfc
Claudio <71095411+itsthered1@users.noreply.github.com>
2023-08-02 01:44:36 -0700

    Initial Commit

---
+++ betterIC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  betterIC
+//
+//  Created by Claudio Rojas on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ betterIC/betterICApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterICApp.swift
+//  betterIC
+//
+//  Created by Claudio Rojas on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct betterICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
